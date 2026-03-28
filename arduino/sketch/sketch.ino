589a6ac58b029314a94129e9f8abb63857a74391
Zoe <tangenzoe@gmail.com>
2026-03-28 15:33:40 +0000

    Can write code to arduino

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -36,6 +36,8 @@ products from Adafruit!
 This code has been adapted for Bath Hack 2026
 */
 /**************************************************************************/
+
+// WRITE
 #include <Wire.h>
 #include <Arduino.h>
 #include <SPI.h>
@@ -76,15 +78,15 @@ void loop(void) {
       uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 
       success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
-
       if (success)
       {
         uint8_t data[16];
 
         // If you want to write something to block 4 to test with, uncomment
         // the following line and this text should be read back in a minute
-        //memcpy(data, (const uint8_t[]){ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0 }, sizeof data);
-        // success = nfc.mifareclassic_WriteDataBlock (4, data);
+        memcpy(data, (const uint8_t[]){ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0 }, sizeof data);
+
+        success = nfc.mifareclassic_WriteDataBlock (4, data);
 
         // Try to read the contents of block 4
         success = nfc.mifareclassic_ReadDataBlock(4, data);
@@ -111,5 +113,87 @@ void loop(void) {
 
 
 
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 
+// READING:
+// #include <Wire.h>
+// #include <Arduino.h>
+// #include <SPI.h>
+// #include <Adafruit_PN532.h>
+//
+// #define PN532_IRQ   (2)
+// #define PN532_RESET (3) 
+//
+// // Shield with an I2C connection:
+// Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+//
+//
+// void setup(void)
+// {
+//     Serial.begin(115200);
+//     Wire.begin();
+//     SPI.begin();
+//     nfc.begin();
+//
+//     uint32_t versiondata = nfc.getFirmwareVersion();
+//     if (!versiondata) {
+//         Serial.println("Didn't find PN53x board");
+//         while (1);
+//     }
+//     nfc.SAMConfig();
+// }
+//
+//
+// void loop(void) {
+//   uint8_t success;
+//   uint8_t uid[7];
+//   uint8_t uidLength;  
+//   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+//
+//   if (success) {
+//     if (uidLength == 4)
+//     {
+//       uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+//
+//       success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
+//
+//       if (success)
+//       {
+//         uint8_t data[16];
+//
+//         // If you want to write something to block 4 to test with, uncomment
+//         // the following line and this text should be read back in a minute
+//         //memcpy(data, (const uint8_t[]){ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0 }, sizeof data);
+//         // success = nfc.mifareclassic_WriteDataBlock (4, data);
+//
+//         // Try to read the contents of block 4
+//         success = nfc.mifareclassic_ReadDataBlock(4, data);
+//         if (success)
+//         {
+//           nfc.PrintHexChar(data, 16); // Print the data
+//           Serial.println("OK");
+//
+//           // Wait a bit before reading the card again
+//           // delay(200);
+//         }
+//         else
+//         {
+//           Serial.println("ERROR");
+//         }
+//       }
+//       else
+//       {
+//         Serial.println("ERROR");
+//       }
+//     }
+//   }
+// }
+//
+//
+//
+//
+//
