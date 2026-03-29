780b5b9ac6df100009b700835fc3649d08c53373
Zoe <tangenzoe@gmail.com>
2026-03-29 03:02:02 +0100

    Mixing bowl listens for cards

--- godot/scenes/listener.gd
+++ godot/scenes/listener.gd
@@ -4,9 +4,9 @@ var udp = PacketPeerUDP.new()
 
 func _ready():
 	udp.bind(9999)
-	print("Listening on UDP 9999...")
+	print("Listening")
 
 func _process(_delta):
 	while udp.get_available_packet_count() > 0:
 		var msg = udp.get_packet().get_string_from_utf8()
-		print("Received: ", msg)
+		print(msg)
