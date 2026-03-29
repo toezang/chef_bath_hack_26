7b6719ef8eb9e0b317796cd40bd51dafe098bf12
toez <156141332+toezang@users.noreply.github.com>
2026-03-29 07:41:33 +0100

    Revert "Rfid game"

--- godot/scenes/listener.gd
+++ godot/scenes/listener.gd
@@ -1,17 +1,12 @@
 extends Node
 
-signal message_received(msg: String)
-
 var udp = PacketPeerUDP.new()
 
 func _ready():
 	udp.bind(9999)
-	print("Listening")
+	print("Listening on UDP 9999...")
 
 func _process(_delta):
 	while udp.get_available_packet_count() > 0:
-		var packet = udp.get_packet()
-		var msg = packet.get_string_from_ascii()
-		var clean = msg.replace(char(0), "").strip_edges()
-		print("CLEAN: [", clean, "]")
-		message_received.emit(clean)
+		var msg = udp.get_packet().get_string_from_utf8()
+		print("Received: ", msg)
